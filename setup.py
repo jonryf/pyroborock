be182e9e127b5d8ad018ee56887a4c4c31cf08cf
Jon <jon.ryfetten@outlook.com>
2022-06-11 16:45:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ def read(fname):
 
 setup(
     name='pyroborock',
-    version='1.1.2',
+    version='1.1.3',
     packages=['pyroborock'],
-    install_requires=['tinytuya==v1.2.3', 'python-miio==0.5.6', 'pycryptodome==3.9.8'],
+    install_requires=['tinytuya==v1.2.3', 'python-miio==0.5.11', 'pycryptodome==3.14.1'],
     description='Communicate with roborock over tuya protocol',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
